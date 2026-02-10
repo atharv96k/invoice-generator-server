fd14a7a8fabb5ba386f97b8f05fab0aaf3ac0434
atharv96k <atharvmohite96k@gmail.com>
2026-02-10 16:34:31 +0530

    feat: create API to delete invoice using id
    Signed-off-by: atharv96k <atharvmohite96k@gmail.com>

--- src/main/java/com/master/invoicegeneratorapi/controller/InvoiceController.java
+++ src/main/java/com/master/invoicegeneratorapi/controller/InvoiceController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +33,12 @@ public class InvoiceController {
 	@GetMapping
 	public ResponseEntity<List<Invoice>> fetchInvoice() {
 		return ResponseEntity.ok(invoiceService.fetchInvoice());
+	}
+	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> removeInvoice(@PathVariable String id) {
+		invoiceService.removeInvoice(id);
+		return ResponseEntity.noContent().build();
 	}
 }
